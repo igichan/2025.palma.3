2ef947f40ce6cf30b8e028328b9c273d0ad9f9df
igichan <igichan52@gmail.com>
2025-03-11 20:08:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ st.title("팔마 3학년 시간표")
 # 반 선택
 class_number = st.selectbox("반을 선택하세요", list(timetable_template.keys()))
 
-if class_number:
+if end class_number:
     # 과목 및 교실 정보 입력
     subject_mapping = {}
     classroom_mapping = {}
